5f6e8e3d7b9bf0d0d4cb0dc4358b96ed35e21a9a
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-01-17 15:47:24 +0100

    Rearrange distinction new_adagraph_design and adagraph_design, add validate_adagraph_design_params

--- R/adagraph_design.R
+++ R/adagraph_design.R
@@ -3,27 +3,55 @@ new_adagraph_design <- function(
     correlation=matrix(),
     weights=double(),
     alpha=double(),
-    k=int(),
-    hypMatrix=matrix(),
-    weightsMatrix=matrix(),
-    closedMatrix=matrix(),
+    test_m=matrix(),
     ...,
     class = character()
 ) {
-    print(weights)
+    k <- dim(correlation)[1]
+    #generate weights for all sub-hypotheses
+    graph <- new("graphMCP", m=test_m, weights=weights) 
+    temp <- gMCPLite::generateWeights(graph)
+    hypMatrix <- temp[,1:k]
+    weightsMatrix <- temp[,(k+1):(2*k)]
+    closedMatrix=matrix(NA,nrow=2^(k-1),ncol=k)
+    for (i in 1:k) {
+        # Fill the result matrix with row indices
+        closedMatrix[,i]=which(hypMatrix[,i]==1)
+    }
+
     design <- list(
         correlation=correlation,
         weights=weights,
         alpha=alpha,
         hypMatrix=hypMatrix,
         weightsMatrix=weightsMatrix,
-        closedMatrix=closedMatrix
+        closedMatrix=closedMatrix,
+        test_m=test_m
     )
     structure(
         design,
         k = k,
         class = c(class, "adagraph_design")
     )
+}
+
+validate_adagraph_design_params <- function(
+    correlation=matrix(),
+    weights=double(),
+    alpha=double(),
+    test_m=matrix()
+) {
+    if (!is.matrix(correlation) | dim(correlation)[1] != dim(correlation)[2]) {
+        stop("correlation has to be a quadratic matrix")
+    } 
+    k <- dim(correlation)[1]
+    if (length(weights) != k) {
+        stop("Need exactly one weight per hypothesis")
+    } else if (!is.numeric(alpha) | alpha<0 | alpha>1) {
+        stop("alpha has to be numeric between 0 and 1")
+    } else if (!is.matrix(test_m) | ((dim(test_m)[1] != dim(test_m)[2]) | (dim(test_m)[1] != k))) {
+       stop("test_m needs to be a quadratic matrix with dimension same as the number of hypotheses")
+    }
 }
 
 #' Make a new (generic) trial design
@@ -52,37 +80,16 @@ adagraph_design <- function(
     alpha=double(),
     test_m=matrix()
 ) {
-    #simple validations
-    if (dim(correlation)[1] == dim(correlation)[2]) {
-        k <- dim(correlation)[1]
-    } else {
-        stop(
-            "correlation has to be a quadratic matrix")
-    }
-    if (length(weights) != k) {
-        stop("Need exactly one weight per hypothesis")
-    } else if ((dim(test_m)[1] != dim(test_m)[2]) | (dim(test_m)[1] != k)) {
-       stop("test_m needs to be a quadratic matrix with dimension same as the number of hypotheses")
-    }
-
-    #generate weights for all sub-hypotheses
-    graph <- new("graphMCP", m=test_m, weights=weights) 
-    temp <- gMCPLite::generateWeights(graph)
-    hypMatrix <- temp[,1:k]
-    weightsMatrix <- temp[,(k+1):(2*k)]
-    closedMatrix=matrix(NA,nrow=2^(k-1),ncol=k)
-    for (i in 1:k) {
-        # Fill the result matrix with row indices
-        closedMatrix[,i]=which(hypMatrix[,i]==1)
-    }
-
+    validate_adagraph_design_params(
+        correlation = correlation,
+        weights = weights,
+        alpha = alpha,
+        test_m = test_m
+    )
     new_adagraph_design(
         correlation = correlation,
         weights = weights,
         alpha = alpha,
-        k = k,
-        hypMatrix = hypMatrix,
-        weightsMatrix = weightsMatrix,
-        closedMatrix = closedMatrix
+        test_m = test_m
     )
 }
