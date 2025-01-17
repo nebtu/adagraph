fa90a62be9becea91f8fcad99dc8f2061e0af875
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-01-17 17:18:08 +0100

    Added test for adagraph_design

---
+++ tests/testthat/test-adagraph_design.R
@@ -0,0 +1,64 @@
+test_that("Simple design works", {
+  correlation <- rbind(H1=c(1, NA),
+                       H2=c(NA, 1))
+  weights <- c(2/3, 1/3)
+  test_m <- rbind(c(0, 1),
+                  c(1, 0))
+  alpha <- 0.05
+  design <- adagraph_design(
+     correlation=correlation,
+     weights=weights,
+     alpha=alpha,
+     test_m=test_m)
+  expect_equal(design$correlation, correlation)
+  expect_equal(design$weights, weights)
+  expect_equal(design$test_m, test_m)
+  expect_equal(design$alpha, alpha)
+  expect_equal(unname(design$hypMatrix), rbind(c(0,1), c(1,0), c(1,1)))
+  expect_equal(unname(design$weightsMatrix), rbind(c(0,1), c(1,0), c(2/3, 1/3)))
+  expect_equal(design$closedMatrix, rbind(c(2,1), c(3,3)))
+})
+
+test_that("Correct validation of adagraph_design", {
+  correlation <- rbind(H1=c(1, NA),
+                       H2=c(NA, 1))
+  weights <- c(2/3, 1/3)
+  test_m <- rbind(c(0, 1),
+                  c(1, 0))
+  alpha <- 0.05
+  expect_error(adagraph_design(correlation = rbind(c(1,NA)),
+                               weights = weights,
+                               alpha = alpha,
+                               test_m = test_m),
+               class="invalid_argument_correlation")
+  expect_error(adagraph_design(correlation = "correlation",
+                               weights = weights,
+                               alpha = alpha,
+                               test_m = test_m),
+               class="invalid_argument_correlation")
+  expect_error(adagraph_design(correlation = correlation,
+                               weights = 1,
+                               alpha = alpha,
+                               test_m = test_m),
+               class="invalid_argument_weights")
+  expect_error(adagraph_design(correlation = correlation,
+                               weights = weights,
+                               alpha = "0.005",
+                               test_m = test_m),
+               class="invalid_argument_alpha")
+  expect_error(adagraph_design(correlation = correlation,
+                               weights = weights,
+                               alpha = 1.005,
+                               test_m = test_m),
+               class="invalid_argument_alpha")
+  expect_error(adagraph_design(correlation = correlation,
+                               weights = weights,
+                               alpha = alpha,
+                               test_m = "test_m"),
+               class="invalid_argument_test_m")
+  expect_error(adagraph_design(correlation = correlation,
+                               weights = weights,
+                               alpha = alpha,
+                               test_m = rbind(c(1,0,0), c(1,0,0), c(1,0,0))),
+               class="invalid_argument_test_m")
+})
