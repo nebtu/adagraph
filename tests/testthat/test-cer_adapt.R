20b45601218f6c10725828d5be608721daa9cbfd
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-03-14 17:05:55 +0100

    Some tests for adapting designs

---
+++ tests/testthat/test-cer_adapt.R
@@ -0,0 +1,70 @@
+
+test_that("dropping hypotheses works", {
+    design <- make_example_design()
+    design <- cer_interim_test(design, c(0.00045, 0.0952, 0.0225, 0.1104))
+
+    design_adj <- cer_drop_hypotheses(design, c(TRUE, FALSE, FALSE, FALSE))
+
+    expect_equal(design_adj$ad_weights, c(0, 0.75, 0.25, 0))
+
+    expect_equal(unname(design_adj$ad_test_m),
+        rbind(c(0, 0, 0, 0),
+              c(0, 0, 1/3, 2/3),
+              c(0, 1, 0, 0),
+              c(0, 1/2, 1/2, 0))
+      )
+
+    design_adj <- cer_drop_hypotheses(design, c(TRUE, FALSE, TRUE, FALSE))
+
+    expect_equal(design_adj$ad_weights, c(0, 1, 0, 0))
+    expect_equal(unname(design_adj$ad_test_m),
+        rbind(c(0, 0, 0, 0),
+              c(0, 0, 0, 1),
+              c(0, 0, 0, 0),
+              c(0, 1, 0, 0))
+      )
+
+    design_adj <- cer_adapt(design_adj, weights=c(0, 0.5, 0, 0.5), t=0.4)
+
+    expect_equal(design_adj$ad_t, 0.4)
+    expect_equal(design_adj$ad_weights, c(0, 0.5, 0, 0.5))
+    
+    design_with_changes <- cer_design(
+        correlation = design$correlation,
+        weights = c(0, 0.5, 0, 0.5),
+        alpha = design$alpha,
+        test_m = design_adj$ad_test_m,
+        alpha_spending_f = design$alpha_spending_f,
+        t = 0.4
+    )
+
+    expect_equal(design_adj$ad_weights_matrix, design_with_changes$weights_matrix)
+})
+
+test_that("multiple adaptions work the same as one adaption", {
+    design <- make_example_design()
+    design <- cer_interim_test(design, c(0.00045, 0.0952, 0.0225, 0.1104))
+
+    expect_equal(
+        cer_drop_hypotheses(
+            cer_drop_hypotheses(design, c(TRUE, FALSE, FALSE, FALSE)),
+            c(FALSE, TRUE, FALSE, FALSE)),
+        cer_drop_hypotheses(design, c(TRUE, TRUE, FALSE, FALSE))
+    )
+
+    new_test_m <- rbind(
+        H1=c(0,1/3,2/3,0),
+        H2=c(1/3,0,0,2/3),
+        H3=c(0,1/2,0,1/2),
+        H4=c(1/2,0,1/2,0)
+    )
+    expect_equal(
+        cer_adapt(
+            cer_adapt(cer_adapt(design, test_m=new_test_m),
+                weights=c(0, 0.5, 0, 0.5)),
+            t=0.4),
+        cer_adapt(design, weights=c(0, 0.5, 0, 0.5), test_m=new_test_m, t=0.4),
+        list_as_map = TRUE
+    )
+
+})
