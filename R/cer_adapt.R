c18e97b84dc50922ab9b4de50d3a75858fa294e9
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-03-20 10:57:41 +0100

    Alternative function for dropping hypotheses

--- R/cer_adapt.R
+++ R/cer_adapt.R
@@ -92,7 +92,6 @@ cer_drop_hypotheses <- function(
 ) {
    hypotheses <- ifelse(hypotheses, 0, 1)
    hyp_index <- which(sapply(asplit(design$hyp_matrix, 1), function(x) all(x == hypotheses)))
-   #intersection_hyp <- which(rowSums(design$hyp_matrix[,hyp, drop=FALSE]) == 0)
    if (!is.null(design$ad_weights_matrix)) {
     weights <- design$ad_weights_matrix[hyp_index, ]
    } else {
@@ -122,3 +121,54 @@ cer_drop_hypotheses <- function(
    }
    cer_adapt(design, weights = weights, test_m = test_m)
 }
+
+#' Adapt a cer design by dropping hypotheses, with a simplified strategy for redistributing weights
+#'
+#' The weights of the dropped hypotheses are set to 0 and distributed proportionally to the previous weight to the other hypotheses.
+#' This means the weights "stay the same", they are only adapted to still sum up to 1.
+#' Note that this method does not lead to an adapted graph that is coherent with the actual weight distribution, this may lead to problems down the line
+#' 
+#' @param design cer_design object
+#' @param hypotheses vector of booleans indicating for each hypotheses if it should be dropped
+#'
+#' @return design with specified hypotheses dropped (so TRUE means the hypothesis is dropped)
+#' @export
+#'
+#' @examples
+#' as <- function(x,t) 2-2*pnorm(qnorm(1-x/2)/sqrt(t))
+#' design <- cer_design(
+#'  correlation=rbind(H1=c(1, NA),
+#'                    H2=c(NA, 1)),
+#'  weights=c(2/3, 1/3),
+#'  alpha=0.05,
+#'  test_m=rbind(c(0, 1),
+#'               c(1, 0)),
+#'  alpha_spending_f=as,
+#'  t=0.5)
+#' 
+#' design <- cer_interim_test(design, c(0.1, 0.02))
+#' 
+#' design <- cer_alt_drop_hypotheses(design, c(TRUE, FALSE))
+#' design
+cer_alt_drop_hypotheses <- function(
+    design,
+    hypotheses
+) {
+    weights <- design$weights
+    weights[hypotheses] <- 0
+    weights <- weights / sum(weights)
+    weights_matrix <- design$weights_matrix
+    weights_matrix[, hypotheses] <- 0
+    weights_matrix <- t(apply(weights_matrix, 1, function(row) {
+        if (sum(row) == 0) {
+            return(row)
+        } else {
+            return(row / sum(row))
+        }
+    }))
+
+    design$ad_weights <- weights
+    design$ad_weights_matrix <- weights_matrix
+    design
+}
+
