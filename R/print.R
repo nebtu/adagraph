96c056c6ab6e236c149a79bc29321d2512146196
Benjamin F <60304037+nebtu@users.noreply.github.com>
2025-08-05 23:34:05 +0200

    New print method for cer_design objects

---
+++ R/print.R
@@ -0,0 +1,73 @@
+#' @export
+print.cer_design <- function(design) {
+  k <- attr(design, "k")
+  cli::cat_line(
+    "A CER Design object, for testing ",
+    k,
+    " hypotheses at FWER ",
+    design$alpha,
+    "."
+  )
+  cli::cat_line()
+  invisible(design)
+
+  cli::cat_rule("Inital design specification")
+  cli::cat_line()
+
+  cli::cat_line("Hypotheses weights")
+  cli::cat_print(design$weights)
+  cli::cat_line()
+
+  cli::cat_line("Graph Transition Matrix")
+  cli::cat_print(design$test_m)
+  cli::cat_line()
+
+  if (!all(is.na(design$correlation))) {
+    cli::cat_line("Correlation for parametric test")
+    cli::cat_print(design$correlation)
+    cli::cat_line()
+  }
+
+  cli::cat_line("Interim test is planned at time fraction ", design$t)
+  cli::cat_line()
+
+  if (design$interim_test) {
+    cli::cat_rule("Interim test result")
+    cli::cat_line("")
+
+    cli::cat_line("P-values of interim test are ", design$p_values_interim)
+    if (any(design$rej_interim)) {
+      cli::cat_line(
+        "Hypotheses rejected at the interim: ",
+        which(design$rej_interim)
+      )
+    } else {
+      cli::cat_line("No Hypotheses were rejected at the interim")
+    }
+    cli::cat_line("")
+  }
+
+  if (design$adaptions) {
+    cli::cat_rule("Adaptions from inital specification")
+    cli::cat_line("")
+    if (design$ad_weight != design$weights) {
+      cli::cat_line("New hypotheses weights")
+      cli::cat_print(design$ad_weights)
+      cli::cat_line()
+    }
+    if (design$ad_test_m != design$test_m) {
+      cli::cat_line("New graph Transition Matrix")
+      cli::cat_print(design$ad_test_m)
+      cli::cat_line()
+    }
+    if (design$ad_correlation != design$correlation) {
+      cli::cat_line("New correlation for parametric test")
+      cli::cat_print(design$adcorrelation)
+      cli::cat_line()
+    }
+    if (design$ad_t != design$t) {
+      cli::cat_line("Interim test was done at time fraction ", design$t)
+      cli::cat_line("")
+    }
+  }
+}
