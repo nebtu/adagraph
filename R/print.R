65845ef2353f56963e84aa77152e728307991047
Benjamin Fallmann <benjamin.fallmann@meduniwien.ac.at>
2025-08-22 17:04:52 +0200

    Fix print for cer_design objects

--- R/print.R
+++ R/print.R
@@ -35,7 +35,8 @@ print.cer_design <- function(design) {
     cli::cat_rule("Interim test result")
     cli::cat_line("")
 
-    cli::cat_line("P-values of interim test are ", design$p_values_interim)
+    cli::cat_line("P-values of interim test are:")
+    cli::cat_print(design$p_values_interim)
     if (any(design$rej_interim)) {
       cli::cat_line(
         "Hypotheses rejected at the interim: ",
@@ -49,25 +50,42 @@ print.cer_design <- function(design) {
 
   if (design$adaptions) {
     cli::cat_rule("Adaptions from inital specification")
-    cli::cat_line("")
-    if (design$ad_weight != design$weights) {
+    cli::cat_line()
+    if (!identical(design$ad_weights, design$weights)) {
       cli::cat_line("New hypotheses weights")
       cli::cat_print(design$ad_weights)
       cli::cat_line()
     }
-    if (design$ad_test_m != design$test_m) {
+    if (!identical(design$ad_test_m, design$test_m)) {
       cli::cat_line("New graph Transition Matrix")
       cli::cat_print(design$ad_test_m)
       cli::cat_line()
     }
-    if (design$ad_correlation != design$correlation) {
+    if (!identical(design$ad_correlation, design$correlation)) {
       cli::cat_line("New correlation for parametric test")
-      cli::cat_print(design$adcorrelation)
+      cli::cat_print(design$ad_correlation)
       cli::cat_line()
     }
-    if (design$ad_t != design$t) {
-      cli::cat_line("Interim test was done at time fraction ", design$t)
-      cli::cat_line("")
+    if (!identical(design$ad_t, design$t)) {
+      cli::cat_line("New time fractions for the hypotheses")
+      cli::cat_print(design$ad_t)
+      cli::cat_line()
+    }
+  }
+
+  if (design$final_test) {
+    cli::cat_rule("Final test result")
+    cli::cat_line("")
+
+    cli::cat_line("Overall p-values of the hypotheses are:")
+    cli::cat_print(design$p_values_final)
+    if (any(design$rej)) {
+      cli::cat_line(
+        "Hypotheses rejected: ",
+        which(design$rej)
+      )
+    } else {
+      cli::cat_line("No Hypotheses were rejected")
     }
   }
 }
