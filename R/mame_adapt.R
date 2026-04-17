04f2107276de3346bc4b889111502ccdaa0ff463
Benjamin F. <benjamin@ims-63-067>
2026-04-17 17:11:46 +0200

    Documentation for mame_adapt functions

--- R/mame_adapt.R
+++ R/mame_adapt.R
@@ -1,9 +1,104 @@
+#' Adapt the patient numbers for the second stage of a MAME design
+#'
+#' Without any changes, `adagraph` expects the correlation between the p-values
+#' to be the same as in the first stage. If there are changes in the relative
+#' sample sizes, either because of changes in the design or random variations,
+#' this might not be accurate. Additionally, one might want to use a different t
+#' for calculating the combined p-value, using the information fraction of the
+#' actual realised patient numbers.
+#'
+#' @param design The `mame_design` to be adapted
+#' @param n_control_2 The patient numbers in the control. Can only be used if
+#'   there are no subgroups
+#' @param n_arms_2 The patient numbers in the arms (as a numberic vector). Can only be used if
+#'   there are no subgroups
+#' @param ad_n_subgroups A data.frame specifying the structure of the subgroups,
+#'   see details for more information
+#' @param calculate_t Calculate a new information fraction t, and adapt the
+#'   design to use it.
+#' @param adapt_bounds Adapt the bounds for rejecting a hypotheses to keep the
+#'   FWER with the new adaptions. If doing multiple adaptions, it is enough to
+#'   adapt bounds only for the last one, or call `adapt_bounds()` manually
+#'   after.
+#'
+#' @return An object of class `mame_design`, with the adaptions applied.
+#'
+#' @examples
+#' m <- rbind(
+#'   c(0, 1 / 2, 1 / 2, 0),
+#'   c(1 / 2, 0, 0, 1 / 2),
+#'   c(0, 1, 0, 0),
+#'   c(1, 0, 0, 0)
+#' )
+#' as <- function(x, t) 2 - 2 * stats::pnorm(stats::qnorm(1 - x / 2) / sqrt(t))
+#'
+#' des <- mame_design(
+#'   arms = 2,
+#'   endpoints = 2,
+#'   n_control = 35,
+#'   n_arms = 35,
+#'   weights = c(1 / 2, 1 / 2, 0, 0),
+#'   t = 0.5,
+#'   alpha = 0.025,
+#'   test_m = m,
+#'   alpha_spending_f = as
+#' ) |>
+#'   cer_interim_test(c(
+#'    0.00045,
+#'    0.0952,
+#'    0.0225,
+#'    0.1104
+#'  ))
+#'
+#' des_ad <- des |>
+#'   mame_adapt_n(n_control_2 = 50, n_arms = c(20, 30))
+#'
+#' des_ad
+#' des_ad$ad_correlation #changed correlation structure
+#' des_ad$ad_t #changed t (different for each hypothesis)
+#'
+#' n_subgroups <- rbind(
+#'   data.frame(arm = "control", `G1` = FALSE, n = 20),
+#'   data.frame(arm = "control", `G1` = TRUE, n = 15),
+#'   data.frame(arm = "A1", `G1` = FALSE, n = 20),
+#'   data.frame(arm = "A1", `G1` = TRUE, n = 15)
+#' )
+#' des <- mame_design(
+#'   endpoints = 2,
+#'   subgroups = 1,
+#'   n_subgroups = n_subgroups,
+#'   weights = c(1 / 2, 1 / 2, 0, 0),
+#'   t = 0.5,
+#'  alpha = 0.025,
+#'  test_m = m,
+#'  alpha_spending_f = as
+#' ) |>
+#'   cer_interim_test(c(
+#'     0.00045,
+#'     0.0952,
+#'     0.0225,
+#'     0.1104
+#'   ))
+#'
+#' ad_n_subgroups <- rbind(
+#'   data.frame(arm = "control", `G1` = FALSE, n = 30),
+#'   data.frame(arm = "control", `G1` = TRUE, n = 20),
+#'   data.frame(arm = "A1", `G1` = FALSE, n = 10),
+#'   data.frame(arm = "A1", `G1` = TRUE, n = 15)
+#' )
+#' des_ad <- des |>
+#'   mame_adapt_n(ad_n_subgroups = ad_n_subgroups)
+#'
+#' des_ad
+#'
+#' des_ad$ad_correlation #changed correlation structure
+#' des_ad$ad_t #changed t (different for each hypothesis)
 #' @export
 mame_adapt_n <- function(
   design,
   n_control_2 = NULL,
   n_arms_2 = NULL,
-  n_subgroups_2 = NULL,
+  ad_n_subgroups = NULL,
   calculate_t = TRUE,
   adapt_bounds = TRUE
 ) {
@@ -16,12 +111,12 @@ mame_adapt_n <- function(
       cli::cli_warn(
         "The {.arg n_control_2} and {.arg n_arms_2} arguments are only supported for designs without subgroups."
       )
-    } else if (!is.null(n_subgroups_2)) {
+    } else if (!is.null(ad_n_subgroups)) {
       cli::cli_warn(
-        "{.arg n_control_2} and {.arg n_arms_2} are ignored since {.arg n_subgroups_2} was supplied"
+        "{.arg n_control_2} and {.arg n_arms_2} are ignored since {.arg ad_n_subgroups} was supplied"
       )
     } else {
-      n_subgroups_2 <- data.frame(
+      ad_n_subgroups <- data.frame(
         arm = c("control", design[["names_arms"]]),
         n = c(n_control_2, n_arms_2)
       )
@@ -32,14 +127,14 @@ mame_adapt_n <- function(
     design[["arms"]],
     design[["endpoints"]],
     design[["subgroups"]],
-    n_subgroups_2,
+    ad_n_subgroups,
     design[["names_arms"]],
     design[["names_subgroups"]]
   )
 
   if (calculate_t) {
     total_subgroup_2 <- get_total_subgroup(
-      n_subgroups_2,
+      ad_n_subgroups,
       design[["names_arms"]],
       design[["names_subgroups"]]
     )
@@ -92,12 +187,95 @@ mame_adapt_n <- function(
     correlation = ad_correlation,
     adapt_bounds = adapt_bounds
   )
-  design_ad$n_subgroups_2 <- n_subgroups_2
+  design_ad$ad_n_subgroups <- ad_n_subgroups
 
   design_ad
 }
 
-#' Drop all hypotheses associated with some groups
+#' Dropp groups, arms or endpoints from a MAME design
+#'
+#' Functions for dropping all hypotheses associated with some subgroups, arms or
+#' endpoints. The weights of the dropped hypotheses are set to 0 and distributed
+#' according to the prespecified graph. The graph is also adapted such that
+#' weights are never assigned to the dropped hypotheses.
+#'
+#' @name mame_drop
+#'
+#' @param design The `mame_design` of which to drop the
+#' @param groups The subgroups to be dropped, by their name or integer indices
+#' @param arms The arms to be dropped, by their name or integer indices
+#' @param endpoints The endpoints to be dropped, by their name or integer indices
+#' @param adapt_bounds Adapt the bounds for rejecting a hypotheses to keep the
+#'   FWER with the new adaptions. If doing multiple adaptions, it is enough to
+#'   adapt bounds only for the last one, or call `adapt_bounds()` manually
+#'   after.
+#'
+#' @return design with the specified hypotheses (according to groups, arms or
+#'   endpoints) dropped
+#'
+#' @examples
+#' m <- rbind(
+#'   c(0, 1 / 2, 1 / 2, 0),
+#'   c(1 / 2, 0, 0, 1 / 2),
+#'   c(0, 1, 0, 0),
+#'   c(1, 0, 0, 0)
+#' )
+#' as <- function(x, t) 2 - 2 * stats::pnorm(stats::qnorm(1 - x / 2) / sqrt(t))
+#'
+#' des <- mame_design(
+#'   arms = 4,
+#'   n_control = 35,
+#'   n_arms = 35,
+#'   weights = c(1/2, 1/2, 0, 0),
+#'   t = 0.5,
+#'   alpha = 0.025,
+#'   test_m = m,
+#'   alpha_spending_f = as
+#' ) |>
+#'  cer_interim_test(c(
+#'    0.00045,
+#'    0.0952,
+#'    0.0225,
+#'    0.1104
+#'  ))
+#'
+#' des_ad <- des |>
+#'   mame_drop_arms(c("A1", "A4"))
+#'
+#' des_ad
+#' des_ad$ad_test_m
+#'
+#' n_subgroups <- rbind(
+#'   data.frame(arm = "control", `G1` = FALSE, n = 20),
+#'   data.frame(arm = "control", `G1` = TRUE, n = 15),
+#'   data.frame(arm = "A1", `G1` = FALSE, n = 20),
+#'   data.frame(arm = "A1", `G1` = TRUE, n = 15)
+#' )
+#' des <- mame_design(
+#'   endpoints = 2,
+#'   subgroups = 1,
+#'   n_subgroups = n_subgroups,
+#'   weights = c(1/2, 1/2, 0, 0),
+#'  t = 0.5,
+#'  alpha = 0.025,
+#'  test_m = m,
+#'  alpha_spending_f = as
+#' ) |>
+#'   cer_interim_test(c(
+#'     0.00045,
+#'     0.0952,
+#'     0.0225,
+#'     0.1104
+#'   ))
+#'
+#' des_ad <- des |>
+#'   mame_drop_groups("G1") |>
+#'   mame_drop_endpoints("E1")
+#'
+#' des_ad
+#' des_ad$ad_test_m
+
+#' @rdname mame_drop
 #' @export
 mame_drop_groups <- function(
   design,
@@ -108,7 +286,7 @@ mame_drop_groups <- function(
   cer_drop_hypotheses(design, drop_hyp, adapt_bounds = adapt_bounds)
 }
 
-#' Drop all hypotheses associated with some arms
+#' @rdname mame_drop
 #' @export
 mame_drop_arms <- function(
   design,
@@ -119,7 +297,7 @@ mame_drop_arms <- function(
   cer_drop_hypotheses(design, drop_hyp, adapt_bounds = adapt_bounds)
 }
 
-#' Drop all hypotheses associated with some endpoints
+#' @rdname mame_drop
 #' @export
 mame_drop_endpoints <- function(
   design,
