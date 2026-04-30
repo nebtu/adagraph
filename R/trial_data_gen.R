c963140cf65b5c126a7f0bb1ccb31bfda72d27cc
Benjamin F. <benjamin@ims-63-067>
2026-04-30 17:53:42 +0200

    First draft for trial_data_gen

---
+++ R/trial_data_gen.R
@@ -0,0 +1,102 @@
+# note that second stage depends on ad_n_table being set
+# TODO: remove n=0 rows in data_spec, make sure that NA is returned for not
+# calculated p-values
+get_trial_data_gen <- function(
+  corr_endpoints,
+  effect_sizes,
+  binary_endpoints = character(0),
+  binary_response = NULL,
+  second_stage = FALSE
+) {
+  function(n, design) {
+    # get the relevant sample sizes
+    if (second_stage) {
+      n_table <- design[["ad_n_table"]]
+    } else {
+      n_table <- design[["n_table"]]
+    }
+
+    # match with the effect sizes
+    data_spec <- merge(
+      n_table,
+      effect_sizes,
+      by = c("arm", design[["names_subgroups"]])
+    )
+
+    # generate data for each group combination
+    data_list <- lapply(seq_len(nrow(data_spec)), function(i) {
+      means <- unlist(data_spec[i, design[["names_endpoints"]]])
+      data <- array(
+        mvtnorm::rmvnorm(
+          data_spec[i, "n"] * n,
+          mean = means,
+          sigma = corr_endpoints
+        ),
+        dim = c(n, data_spec[i, "n"], design[["endpoints"]])
+      )
+      data <- aperm(data, c(2, 3, 1))
+      # now the shape is (number of treated people, number of endpoints, number of runs)
+      data
+    })
+
+    hyp_assoc <- design[["hyp_assoc"]]
+    # test, over each hypothesis separately
+    p_values <- vapply(
+      seq_len(nrow(hyp_assoc)),
+      function(i) {
+        arm <- hyp_assoc[i, "arm"]
+        group <- hyp_assoc[i, "group"]
+        endpoint <- hyp_assoc[i, "endpoint"]
+        endpoint_index <- which(design[["names_endpoints"]] == endpoint)
+        if (group == "Total") {
+          group_filter <- TRUE
+        } else {
+          group_filter <- data_spec[[group]]
+        }
+
+        treat_filter <- data_spec[["arm"]] == arm & group_filter
+        n_treat <- sum(data_spec[treat_filter, "n"])
+        data_treat_list <- data_list[treat_filter]
+        data_treat <- do.call(
+          rbind,
+          lapply(data_treat_list, function(d) {
+            d[, endpoint_index, ]
+          })
+        )
+
+        control_filter <- data_spec[["arm"]] == "control" & group_filter
+        n_control <- sum(data_spec[control_filter, "n"])
+        data_cont_list <- data_list[
+          data_spec[["arm"]] == "control" & group_filter
+        ]
+        data_cont <- do.call(
+          rbind,
+          lapply(data_cont_list, function(d) {
+            d[, endpoint_index, ]
+          })
+        )
+
+        if (endpoint %in% binary_endpoints) {
+          p <- NULL #TODO: implement
+        } else {
+          cont_mean <- colMeans(data_cont)
+          cont_var <- matrixStats::colVars(data_cont)
+          treat_mean <- colMeans(data_treat)
+          treat_var <- matrixStats::colVars(data_treat)
+
+          t <- (treat_mean - cont_mean) /
+            sqrt(
+              (treat_var * (n_treat - 1) + cont_var * (n_control - 1)) /
+                ((n_treat + n_control - 2)) *
+                (1 / n_treat + 1 / n_control)
+            )
+
+          p <- pt(t, df = n_treat + n_control - 2, lower.tail = FALSE)
+        }
+
+        p
+      },
+      numeric(n)
+    )
+  }
+}
